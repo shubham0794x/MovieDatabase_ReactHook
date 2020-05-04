2e2dbc3fcba190adbf64e9aa283c2ed54ad1655e
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-05-04 14:21:00 +0530

    First -Version

--- src/App.js
+++ src/App.js
@@ -13,14 +13,56 @@ function App() {
   });
   const apiurl = "http://www.omdbapi.com/?apikey=d20c1db3";
 
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
 
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })
+      });
+    }
+  }
+  
+  const handleInput = (e) => {
+    let s = e.target.value;
 
+    setState(prevState => {
+      return { ...prevState, s: s }
+    });
+  }
+
+  const openPopup = id => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      console.log(result);
+
+      setState(prevState => {
+        return { ...prevState, selected: result }
+      });
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    });
+  }
 
   return (
     <div className="App">
       <header>
-       <h1>Movie Database</h1>
+        <h1>Movie Database</h1>
       </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+
+        <Results results={state.results} openPopup={openPopup} />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
+      </main>
     </div>
   );
 }
