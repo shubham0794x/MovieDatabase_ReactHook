5891fc81cebb53abf9fda8c707e724117b9a3b76
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-05-28 23:53:26 +0530

    Update Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Popup({ selected, closePopup }) {
 	return (
-		<section className="popup">
+		 < section className="popup" >
 			<div className="content">
 				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
 				<p className="rating">Rating: {selected.imdbRating}</p>
-				<div className="plot">
+				< div className="plot">
 					<img src={selected.Poster} />
 					<p>{selected.Plot}</p>
 				</div>
