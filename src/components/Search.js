58caf1528a1780433839b994dabc0ea2c9b9d6c5
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-05-28 23:54:03 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Search ({ handleInput, search }) {
+function Search ({  handleInput, search  }) {
 	return (
-		<section className="searchbox-wrap">
+		< section className="searchbox-wrap" >
 			<input 
 				type="text" 
 				placeholder="Search for a movie..." 
