637c7c5f4121e216524ed5494e4a938b48ce2c92
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-07 22:56:03 +0530

    Update Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,10 +5,10 @@ function Popup({ selected, closePopup }) {
 		 < section className="popup" >
 			<div className="content">
 				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
-				<p className="rating">Rating: {selected.imdbRating}</p>
+				<p className="rating"> Rating: {selected.imdbRating}</p>
 				< div className="plot">
-					<img src={selected.Poster} />
-					<p>{selected.Plot}</p>
+					<img src= {selected.Poster} />
+					<p> {selected.Plot} </p>
 				</div>
 				<button className="close" onClick={closePopup}>Close</button>
 			</div>
