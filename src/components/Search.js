963348baef7992bd34ded7b3f3c9fd0b7b544641
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-07 22:55:31 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Search ({  handleInput, search  }) {
 	return (
-		< section className="searchbox-wrap" >
+		< section className = "searchbox-wrap" >
 			<input 
 				type="text" 
-				placeholder="Search for a movie..." 
-				className="searchbox" 
-				onChange={handleInput}
-				onKeyPress={search}
+				placeholder = "Search for a movie..." 
+				className= "searchbox" 
+				onChange= {handleInput}
+				onKeyPress ={search}
 			/>
 		</section>
 	)
