8aa7c680d7f7b45ae183ef971d01fa9a6d86d461
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-20 17:20:12 +0530

    Updates

---
+++ precache-manifest.1e878609e3e6962670b9014a2e8fc726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f8906142d2f660a9b8bab696132197",
+    "url": "/MovieDatabase_ReactHook/index.html"
+  },
+  {
+    "revision": "d3f0c47a8b23447988a0",
+    "url": "/MovieDatabase_ReactHook/static/css/main.d8d84540.chunk.css"
+  },
+  {
+    "revision": "a1044735fab9b321580e",
+    "url": "/MovieDatabase_ReactHook/static/js/2.036f29a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieDatabase_ReactHook/static/js/2.036f29a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f0c47a8b23447988a0",
+    "url": "/MovieDatabase_ReactHook/static/js/main.841b65a9.chunk.js"
+  },
+  {
+    "revision": "412422e6727d265e8908",
+    "url": "/MovieDatabase_ReactHook/static/js/runtime-main.17713e07.js"
+  }
+]);
